a09322619a48cf2181775ae65ac2f43b820a7ed6
Tiirnako <treeleaves229@gmail.com>
2024-01-16 23:25:06 -0600

    Refactor begin Properties
    Refactor properties component begin
    
    Remove properties map
    create individual divs for each property

--- frontend/src/Properties.jsx
+++ frontend/src/Properties.jsx
@@ -15,7 +15,11 @@ const Properties = ({ host }) => {
         'Wildrest Allowed: ',
         'Wait Allowed: ',
         'Staff Rank: ',
-    ])
+    ]);
+
+    
+    
+    
     const [staffRank, setStaffRank] = useState(playerData.settings.staffRank);
     const [playerDataStaff, setPlayerDataStaff] = useState(playerData.settings.staffRank);
     
@@ -33,53 +37,65 @@ const Properties = ({ host }) => {
         <div className='font-bold text-xl border-[#ecce24b4] border-b-2 mb-4'>Player Properties</div>
 
         <div className="flex justify-start gap-4">
+
             <div className='flex flex-col w-full'>
-                {properties.map((item, index) => {
-                    if(index <= 3) {
-                        return (
-                            <div key={index}>
-                                {playerProps[index]}{item}
-                            </div>
-                        )
-                    }
-                })}
+
+                <div>
+                    {playerProps[0]}{playerData.settings.difficulty}
+                </div>
+
+                <div>
+                    {playerProps[1]}{playerData.settings.enforcedLogLevel}
+                </div>
+
+                <div>
+                    {playerProps[2]}{playerData.settings.physicsFramerate}
+                </div>
+
+                <div>
+                    {playerProps[3]}{playerData.settings.consoleAllowed}
+                </div>
+
             </div>
+                     
+
             <div className='flex flex-col w-full'>
-                  {properties.map((item, index) => {
-                      if (index === 7) {
 
+                <div>
+                    {playerProps[4]}{playerData.settings.bedRestAllowed}
+                </div>
+                    
+                <div>
+                    {playerProps[5]}{playerData.settings.wildernessRestAllowed}
+                </div>
+                
+                <div>
+                    {playerProps[6]}{playerData.settings.waitAllowed}
+                </div>
+                    
+                  
+                <div onSubmit={e => e.preventDefault()}>
+                        <label htmlFor='staffRank'>{playerProps[7]}</label>
+                        <select onChange={e => {setStaffRank(
+                            {settings:
+                                {staffRank: e.target.value}
+                            }
+                        )}}
+                        defaultValue={staffRank} className='bg-transparent'
+                        name="staffRank"
+                        id="staffRank">
+                            <option className='bg-[#000000af] text-[#8392c5]' value={0}>Player</option>
+                            <option className='bg-[#000000af] text-[#8392c5]' value={1}>Moderator</option>
+                            <option className='bg-[#000000af] text-[#8392c5]' value={2}>Administrator</option>
+                            <option className='bg-[#000000af] text-[#8392c5]' value={3}>Server Owner</option>
+                        </select>
+                </div>
+                  
+                        
+            </div>                       
 
-                          return (<div onSubmit={e => e.preventDefault()} key={index}>
-                              <label htmlFor='staffRank'>{playerProps[index]}</label>
-                              <select onChange={e => {setStaffRank(
-                                    {settings:
-                                        {staffRank: e.target.value}
-                                    }
-                                )}} 
-                                defaultValue={staffRank} className='bg-transparent' 
-                                name="staffRank" 
-                                id="staffRank">
-                                  <option className='bg-[#000000af] text-[#8392c5]' value={0}>Player</option>
-                                  <option className='bg-[#000000af] text-[#8392c5]' value={1}>Moderator</option>
-                                  <option className='bg-[#000000af] text-[#8392c5]' value={2}>Administrator</option>
-                                  <option className='bg-[#000000af] text-[#8392c5]' value={3}>Server Owner</option>
-                              </select>
-                              {playerDataStaff != staffRank ? <button className='border-2 px-2 mt-2 border-[#ecce24da]' onClick={() => {
-                                handleSettingsChange()
-                                
-                                }}>Change</button> : null }
-                          </div>)
-
-                      } else if (index > 3) {
-                          return (
-                            <div key={index}>
-                                {playerProps[index]}{item}
-                            </div>
-                        )
-                    }
-                })}
-            </div>
         </div>
+            {playerDataStaff != staffRank ? <button className='border-2 px-2 mt-2 border-[#ecce24da] self-start' onClick={() => {handleSettingsChange()}}>Change</button> : null }
 
 
     </div>
